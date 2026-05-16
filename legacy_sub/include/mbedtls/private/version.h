5c5996bab0715c7f3139f43b6b8f41b9dce27526
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2026-05-16 23:34:55 +0200

    [nrf noup] Add a version.h
    When legacy crypto is build this header is needed and the option
    to take it from mbedTLS when the rest of the files come from here
    doesn't make sense.
    
    So add a simple implementation of version.h file
    which matches the vesion 4.1.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

---
+++ legacy_sub/include/mbedtls/private/version.h
@@ -0,0 +1,35 @@
+#include <string.h>
+
+/* These come from the mbedtls build_info which we don't use */
+
+/**
+ * The version number x.y.z is split into three parts.
+ * Major, Minor, Patchlevel
+ */
+#define MBEDTLS_VERSION_MAJOR  4
+#define MBEDTLS_VERSION_MINOR  1
+#define MBEDTLS_VERSION_PATCH  0
+
+/**
+ * The single version number has the following structure:
+ *    MMNNPP00
+ *    Major version | Minor version | Patch version
+ */
+#define MBEDTLS_VERSION_NUMBER         0x04010000
+#define MBEDTLS_VERSION_STRING         "4.1.0"
+#define MBEDTLS_VERSION_STRING_FULL    "Mbed TLS 4.1.0"
+
+inline unsigned int mbedtls_version_get_number(void)
+{
+    return MBEDTLS_VERSION_NUMBER;
+}
+
+inline const char *mbedtls_version_get_string(void)
+{
+    return MBEDTLS_VERSION_STRING;
+}
+
+inline const char *mbedtls_version_get_string_full(void)
+{
+    return MBEDTLS_VERSION_STRING_FULL;
+}
